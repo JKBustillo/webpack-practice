aa5b5a5e937d2caf868fdddc05bc83ad851bfa3f
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-04 22:53:37 -0500

    feat: little change to more classy

--- src/components/app-button/appButton.js
+++ src/components/app-button/appButton.js
@@ -1,10 +1,11 @@
-import './AppButton.css';
+import './AppButton.scss';
 
 class AppButton {
+    buttonCssClass = 'app-button';
+    
     render() {
         const button = document.createElement('button');
         button.innerHTML = 'Press me';
-        button.classList.add('app-button');
         const body = document.querySelector('body');
         button.onclick = function () {
             const p = document.createElement('p');
@@ -12,6 +13,7 @@ class AppButton {
             p.classList.add('app-text');
             body.appendChild(p);
         };
+        button.classList.add(this.buttonCssClass);
         body.appendChild(button);
     }
 }
