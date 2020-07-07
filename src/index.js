7ee67d847f8d626f3ec70a3e71b284aa876d9fc7
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-06 22:15:22 -0500

    feat: Heading added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import AppButton from './components/app-button/appButton';
+import Heading from './components/heading/heading';
 // import addImage from './addImage';
 
+const heading = new Heading();
+heading.render();
 const appButton = new AppButton();
 appButton.render();
 // addImage();
