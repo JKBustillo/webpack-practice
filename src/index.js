a71baf45d7e0b08c0b5de9c5e31b1f2b2f111693
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-08 21:10:25 -0500

    feat: mode verification

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ heading.render();
 const appButton = new AppButton();
 appButton.render();
 // addImage();
+
+if (process.env.NODE_ENV === 'development') {
+    console.log('Development build');
+} else if (process.env.NODE_ENV === 'production') {
+    console.log('Production build');
+}
